104dfd6d6eab273abe41d46a30966959116d319c
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-10-21 14:42:40 +0200

    Update lambda.js

--- src/netlify/functions/lambda.js
+++ src/netlify/functions/lambda.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.handler = async function (event, context) {
 
   const API_KEY =  process.env.REACT_API_KEY;
-  const city = event.queryStringParameters.city;
+  const bookName = event.queryStringParameters.bookname;
 
   let uri = "https://www.googleapis.com/books/v1/volumes?q=" + bookName + "&key=" + API_KEY +"&maxResults=40";
 
