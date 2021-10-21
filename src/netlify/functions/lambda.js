496b8aa8f15dd1e93695fb46eb9c23b0f1c3824a
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-10-21 14:52:15 +0200

    Netlify

--- src/netlify/functions/lambda.js
+++ src/netlify/functions/lambda.js
@@ -4,6 +4,7 @@ exports.handler = async function (event, context) {
 
   const API_KEY =  process.env.REACT_API_KEY;
   const bookName = event.queryStringParameters.bookName;
+  console.log("ciuppaaa");
   console.log(API_KEY);
 
   let uri = "https://www.googleapis.com/books/v1/volumes?q=" + bookName + "&key=" + API_KEY +"&maxResults=40";
